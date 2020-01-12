e0a2584126a8631936e93e0a604a2c1a4d1b751d
ardaplun <yulianwardana@gmail.com>
2020-01-12 19:43:03 +0700

    add image placeholder for slow network

--- src/components/PokeCard.tsx
+++ src/components/PokeCard.tsx
@@ -2,13 +2,20 @@ import React from 'react';
 import { Card } from 'semantic-ui-react';
 
 const PokeCard = ({id, name, habitat, onClick}:{id:number, name:string, habitat:string, onClick: ()=>void}) => {
+    let [loaded, setLoaded] = React.useState<boolean>(false)
+
     return(
-        <Card
-            image={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
-            header={name.toUpperCase()}
-            meta={habitat.toUpperCase()}
-            onClick={onClick}
-        />
+        <Card onClick={onClick}>
+            <div className="image">
+                <img src="/default-image.png" style={ loaded ? {display: "none"} : {}} />
+                <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`} onLoad={()=>setLoaded(true)} style={ loaded ? {} : {display: "none"}} />
+            </div>
+            <Card.Content>
+                <Card.Header> {name.toUpperCase()} </Card.Header>
+                <Card.Meta> {habitat.toUpperCase()} </Card.Meta>
+            
+            </Card.Content>
+        </Card>
     )
 }
 
