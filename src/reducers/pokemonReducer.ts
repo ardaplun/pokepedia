c7a41930131c737940c0c6b0ab49792831a8ad32
ardaplun <yulianwardana@gmail.com>
2020-01-12 15:25:17 +0700

    ad minimum reducer so far needed

---
+++ src/reducers/pokemonReducer.ts
@@ -0,0 +1,39 @@
+import { Reducer } from 'redux'
+
+export interface PokemonType{
+    pokemons: undefined | Array<any>
+    pokemon: undefined | Array<any>
+    pokemonHabitats: undefined | Array<any>
+    pokemonSpecies: undefined | Array<any>
+    pokemonShapes: undefined | Array<any>
+    nextUrl: string
+    prevUrl: string
+}
+
+const initialState:PokemonType = {
+    pokemons: undefined,
+    pokemon: undefined,
+    pokemonHabitats: undefined,
+    pokemonSpecies: undefined,
+    pokemonShapes: undefined,
+    nextUrl: '',
+    prevUrl:''
+}
+
+
+const pokemonReducer: Reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'SET_POKEMONS':
+            return { ...state, pokemons: action.data }
+        case 'SET_POKEMON':
+            return { ...state, pokemon: action.data }
+        case 'SET_NEXT_POKEMONS':
+            return { ...state, nextUrl: action.data }
+        case 'SET_PREV_POKEMONS':
+            return { ...state, prevUrl: action.data }
+        default:
+            return state
+    }
+}
+  
+export default pokemonReducer
